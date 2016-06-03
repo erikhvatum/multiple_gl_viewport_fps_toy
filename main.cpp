cbb2ef994c3d47b9b3277187bc30f448e91ab0df
Erik Hvatum <ice.rikh@gmail.com>
2016-06-02 22:16:07 -0500

    Set swap interval to 0.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,21 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSurfaceFormat fmt = QSurfaceFormat::defaultFormat();
+//    fmt.setRenderableType(QSurfaceFormat::OpenGL);
+//    fmt.setProfile(QSurfaceFormat::CompatibilityProfile);
+//    fmt.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+    fmt.setSwapInterval(0);
+//    fmt.setVersion(2, 1);
+//    fmt.setOptions(QSurfaceFormat::DeprecatedFunctions);
+//    fmt.setSamples(8);
+//    fmt.setRedBufferSize(8);
+//    fmt.setGreenBufferSize(8);
+//    fmt.setBlueBufferSize(8);
+//    fmt.setAlphaBufferSize(8);
+    QSurfaceFormat::setDefaultFormat(fmt);
+
     MainWindow w0;
     w0.show();
     MainWindow w1;
